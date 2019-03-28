a0d2349fe4cbaa8c3d38e3a158a90520d8c297bc
Elvin <elvin@athennian.com>
2019-03-28 11:43:57 -0600

    view updates

--- public/controllers/chatCtrl.js
+++ public/controllers/chatCtrl.js
@@ -1,5 +1,9 @@
 view.chatButton()
 
+$(document).on('click', '.box-btn', e => {
+    console.log(e) // im not sure what's inside e, but you'll be able to find the correct ID from here
+})
+
 $('#load-chatbox').click(() => {
     console.log("HELLEREE")
 
