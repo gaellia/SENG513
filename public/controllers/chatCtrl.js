67c38ece399c3cf3e5debd1024fbe43a126de5af
Kevin <poochkevin@hotmail.com>
2019-04-03 21:23:42 -0600

    fixed chat for the new model
    Still need to get displayName and BoxID though

--- public/controllers/chatCtrl.js
+++ public/controllers/chatCtrl.js
@@ -51,34 +51,41 @@ $('#load-chatbox').click(() => {
 
         <!-- Message input -->
         <div class="send">
-            <form actions="">
+            <form id="msg-form" actions="">
                 <input id="m" autocomplete="off" /><button><i class="far fa-paper-plane"></i></button>
             </form>
         </div>
 
     </div>`)
 
-    // testing with the only shoebox that exists lol
-    model.shoebox("vET5va5u3J5V2gPieine").get().then(doc => {
-        if(doc.exists) {
-            const messages = doc.data().messages
-            console.log('messages', messages)
+    // testing with the 'Mexico' shoebox
+    model.shoebox('0nixBz2gEv0vP0yZlY4p').collection('messages').orderBy('timestamp').get().then(snapshot => {
+        snapshot.forEach(doc => {
 
             // show all messages in the shoebox
-            for (let i in messages) {
-                let timestamp = messages[i].timestamp.toDate().toString()
-                timestamp = timestamp.substr(0, timestamp.indexOf(':')+3)   //goes up to the minute 
+            let timestamp = doc.data().timestamp.toDate().toString()
+            timestamp = timestamp.substr(0, timestamp.indexOf(':')+3)   //goes up to the minute 
 
-                $('#chat').append($('<li>').html(`<i class='fas fa-user'><span id='username'>${messages[i].displayName}</span></i><br>
-                                                  <p id='message'>${messages[i].message}</p>
-                                                  <span id='time'>${timestamp}</span>`))
-            }
+            $('#chat').append($('<li>').html(`<i class='fas fa-user'><span id='username'>${doc.data().displayName}</span></i><br>
+                                            <p id='message'>${doc.data().message}</p>
+                                            <span id='time'>${timestamp}</span>`))
+        });
 
-            // scroll to bottom
-            $('#chat').scrollTop($('#chat')[0].scrollHeight);
-
-        } else {
-            console.log("no doc ")
-        }
+        // scroll to bottom
+        $('#chat').scrollTop($('#chat')[0].scrollHeight);
     })
+})
+
+// listener for sending a message
+$(document).on('submit', '#msg-form', e => {
+    e.preventDefault()
+    console.log($('#m').val())
+    let currentTime = firebase.firestore.Timestamp.fromDate(new Date()) // for storing into database
+
+    // Testing
+    let simpleTime = currentTime.toDate().toString()
+    simpleTime = simpleTime.substr(0, simpleTime.indexOf(':')+3)
+    console.log(simpleTime)
+
+    $('#m').val("") // clear send box
 })
