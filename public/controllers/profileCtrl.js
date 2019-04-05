d44826e26e03dc1fe19238aad3f3a855ab5ad52a
Kevin <poochkevin@hotmail.com>
2019-04-05 17:41:33 -0600

    can edit name...sorta
    it changes in the database, but not the original display

--- public/controllers/profileCtrl.js
+++ public/controllers/profileCtrl.js
@@ -14,6 +14,14 @@ $(document).on('click', '#edit-btn', () => {
 // listener for the save button
 $(document).on('click', '#save', () => {
     const user = model.local('user')
-    // update database
-    //model.user(user.uid)
+    if (user.displayName !== $('#edit-name').html()) {
+        model.user().where('uid', '==', user.uid).get().then(response => {
+            response.docs.map(doc => {
+                // update database with edited name
+                model.user(doc.id).update({"displayName": $('#edit-name').html()})
+            })
+        })
+        // update local with edited name
+        model.local('user', {uid: user.uid, displayName: $('#edit-name').html(), email: user.email})
+    }
 })
