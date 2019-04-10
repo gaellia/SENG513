2c4f76bf310a189523ec5170efec2919a2b61066
Kevin <poochkevin@hotmail.com>
2019-04-10 15:48:36 -0600

    a lot of things to settings
    - removed dupe listener
    - changed some stylings
    - fix to local storage
    - added delete box
    - only owners can delete members
    - only owners can delete box
        - they cannot delete themselves from a box they own
    - members can delete themselves

--- public/controllers/authCtrl.js
+++ public/controllers/authCtrl.js
@@ -38,10 +38,6 @@ $(document).on('click', '#profile-btn', () => {
   view.profileModal()
 })
 
-$(document).on('click', '#setting-btn', () => {
-  view.settingsModal()
-})
-
 const authGlobal = {
   // returns shoeboxes of user
   fetchBoxes: function(user) {
